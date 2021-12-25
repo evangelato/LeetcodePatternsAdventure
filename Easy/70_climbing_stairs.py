9f4dc4e11659aee85cf1620dcc48c8a550521721
Evan Hong <evangelurtz@gmail.com>
2021-12-25 18:22:58 +0900

    Modify climbing stairs

--- Easy/70_climbing_stairs.py
+++ Easy/70_climbing_stairs.py
@@ -1,9 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        ways = [0] * (n + 1)
+        if n == 1:
+            return 1
+
+        ways = [0] * (n)
 
         ways[0] = 1
-        ways[1] = 1
+        ways[1] = 2
 
         for i in range(2, len(ways)):
             ways[i] = ways[i - 1] + ways[i - 2]
